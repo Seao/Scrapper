7b9582232ec4d03836991f99290c97211b830fea
Aurélien Wolz <wolzaurelien@gmail.com>
2017-08-29 22:50:33 +0200

    Fix missing query params and add associated tests

--- routes.js
+++ routes.js
@@ -30,29 +30,35 @@ router.get('/', function(req, res, next) {
   // Sanitize query params
   req.sanitizeQuery('url').trim();
   req.sanitizeQuery('xpath').trim();
-  // Get html at the url specified
-  request(req.query.url, function (error, response, body) {
-    // Error handler
-    if (error) {
-      next(error);
-    }
-    // Parser
-    try {
-      var document = parse5.parse(cleanString(body).toString());
-      var xhtml = xmlser.serializeToString(document);
-      var doc = new dom().parseFromString(xhtml);
-      var select = xpath.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
-      var nodes = select(req.query.xpath, doc);
-      // Response
-      res.json({
-        error: false,
-        status: response.statusCode,
-        body: nodes[0] ? nodes[0].toString().replace('xmlns=\"http://www.w3.org/1999/xhtml\"','') : ''
-      });
-    } catch(exception) {
-      next('Unexpected error thrown');
-    }
-  });
+  // Check query params
+  if(req.query.url == undefined || req.query.xpath == undefined) {
+    next('Bad parameters, url or xpath missing');
+  } else {
+    // Get html at the url specified
+    request(req.query.url, function (error, response, body) {
+      // Error handler
+      if (error) {
+        next(error);
+      } else {
+        // Parser
+        try {
+          var document = parse5.parse(cleanString(body).toString());
+          var xhtml = xmlser.serializeToString(document);
+          var doc = new dom().parseFromString(xhtml);
+          var select = xpath.useNamespaces({"x": "http://www.w3.org/1999/xhtml"});
+          var nodes = select(req.query.xpath, doc);
+          // Response
+          res.json({
+            error: false,
+            status: response.statusCode,
+            body: nodes[0] ? nodes[0].toString().replace('xmlns=\"http://www.w3.org/1999/xhtml\"','') : ''
+          });
+        } catch(exception) {
+          next('Unexpected error thrown');
+        }
+      }
+    });
+  }
 });
 
 /*******************************************
--- test.js
+++ test.js
@@ -1,17 +1,37 @@
-process.env.NODE_ENV = 'test';
+/*******************************************
+* DEPENDENCIES
+*******************************************/
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('./index');
-let should = chai.should();
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('./index');
+var should = chai.should();
+
+/*******************************************
+* USAGE
+*******************************************/
 
 chai.use(chaiHttp);
 
+/*******************************************
+* TESTS
+*******************************************/
+
 describe('GET /', () => {
-  it('it should response with status 200', (done) => {
-    chai.request(server)
-    .get('/')
-    .end((err, res) => {
+  it('missing url, it should response with status 500', (done) => {
+    chai.request(server).get('/?xpath=//x:div').end((err, res) => {
+      res.should.have.status(500);
+      done();
+    });
+  });
+  it('missing url, it should response with status 500', (done) => {
+    chai.request(server).get('/?url=http://www.google.fr').end((err, res) => {
+      res.should.have.status(500);
+      done();
+    });
+  });
+  it('valid parameters, it should response with status 200', (done) => {
+    chai.request(server).get('/?url=http://www.google.fr&xpath=//x:div').end((err, res) => {
       res.should.have.status(200);
       done();
     });
